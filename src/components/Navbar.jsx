d814e6e9ef57de5ca4b61ffd3f31dcc7c9f4ec20
Naman Joshi <namanjoshi96@gmial.com>
2025-11-12 13:17:49 +0530

    feat:added Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav style={{ background: '#1e40af', color: 'white', padding: '10px' }}>
+      <h2>React Practice Navbar</h2>
+    </nav>
+  );
+};
+
+export default Navbar;
